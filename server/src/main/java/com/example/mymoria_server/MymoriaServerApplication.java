5feea0bf5db0042f8b002b2cc222367132960d09
Bhadz <darylgzmn@gmail.com>
2025-01-28 14:32:05 -1000

    Adding server directories

---
+++ server/src/main/java/com/example/mymoria_server/MymoriaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymoria_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymoriaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymoriaServerApplication.class, args);
+	}
+
+}
